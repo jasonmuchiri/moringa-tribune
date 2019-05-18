095316a8f4f7b0b31a28266500a92cc9faa34914
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-18 23:52:57 +0300

    Deploy changes to heroku

--- news/models.py
+++ news/models.py
@@ -1,25 +1,9 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
-    email = models.EmailField()
-    
-
-    def __str__(self):
-        return self.first_name
-
-    def save_editor(self):
-        self.save()
-
-    def delete_editor(self):
-        Editor.objects.all().delete()    
-
-    class Meta:
-        ordering = ['first_name']
-    
 
 class tags(models.Model):
     name = models.CharField(max_length = 30)
@@ -29,8 +13,8 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length = 60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to = 'articles/',blank=True)               
@@ -53,3 +37,7 @@ class Article(models.Model):
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news    
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()      
--- news/tests.py
+++ news/tests.py
@@ -1,26 +1,8 @@
 from django.test import TestCase
-from .models import Editor,Article,tags
+from .models import Article,tags
 import datetime as dt
 
 # Create your tests here.
-class EditorTestClass(TestCase):
-
-    def setUp(self):
-        self.james = Editor(first_name ='James', last_name = 'Muriuki', email = 'james@moringaschool.com')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.james,Editor))
-
-    def test_save_method(self):
-        self.james.save_editor()
-        editors = Editor.objects.all()
-        self.assertTrue(len(editors) > 0)
-
-    def test_delete_method(self):
-        self.james.delete_editor()
-        editors = Editor.objects.all()
-        self.assertTrue(len(editors) == 0)            
-
 class ArticleTestClass(TestCase):
 
     def setUp(self):
